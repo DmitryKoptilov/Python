12c1bc3efcba74ef9ba18e7494830b6c6bdb98f4
DmytroKoptilov <dmitrykoptilovqa@gmail.com>
2023-02-27 21:23:35 +0200

    Homework_9

---
+++ homework_9.py
@@ -0,0 +1,28 @@
+string = "Hello, world!"
+
+# a
+print(string[2])
+
+# b
+print(string[-2])
+
+# c
+print(string[:5])
+
+# d
+print(string[:-2])
+
+# e
+print(string[::2])
+
+# f
+print(string[1::2])
+
+# g
+print(string[::-1])
+
+# h
+print(string[-1::-2])
+
+# i
+print(len(string))
